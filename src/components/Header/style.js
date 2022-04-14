4f758ca793a9fce090d8f81ff1a3274d5e96eb2f
gserpeloni <gserpeloni@hotmail.com>
2022-04-13 22:22:52 -0300

    chore: adjust lint and organize code

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,61 +1,24 @@
 import styled from 'styled-components';
 
-
 export const Navbar = styled.div`
-  width: 100%;
-  position: fixed; 
+  z-index: 1;
+  /* position: fixed;  */
   display: flex;
-  flex-direction: row;
-  flex-flow: wrap;
-
-
-
-
-  font-family:'Courier New', Courier, monospace;
-  font-weight: bold;
-  font-size: 1.2rem;
-  color: white;
-
-  
   height: 7vh;
   
-  z-index: 1;
-
-  background-color: #23272E;
-  opacity: 80%;
-
   align-items: center;
 
-  div:hover{
-    color: goldenrod;
-  };
-
-  span{
-    font-size: 1.6rem;
-  }
+  font-size: 1.8rem;
+  color: black;
 
   div{
     text-align: right;
     padding-left: 10px;
     padding-right: 10px;
-
   }
 
-  .nameDiv{
-    flex: 1 40%;
-    text-align: left;
-    padding-left: 80px;
-  }
-
-
-  @media(max-width: 650px){
-    font-size: 1.0rem;
-
-    .nameDiv{
-       font-size: 0.2rem;
-       flex: 1 0%;
-       padding: 0;
-    }
-  }
+  div:hover{
+    color: goldenrod;
+  };
 
 `;
