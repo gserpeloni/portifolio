10f046ebc450a62d1ddae60c7b50b8ee3defd2b9
gserpeloni <gserpeloni@hotmail.com>
2022-05-04 20:33:11 -0300

    chore: adjust styles and try to adjust app.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,14 +6,15 @@ import Header from '../components/Header';
 import About from '../components/About';
 import './App.css';
 
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <Navbar />
       <Header />
       {/* <About></About>
-       <Projects></Projects>
-       <Footer></Footer>  */}
+       <Projects></Projects>*/}
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 
 export const FooterDiv = styled.div`
- 
-  height: 10vh;
-  background-color: rgba(0,0,0,0.9);
+  height: auto;
+  /* background-color: rgba(0,0,0,0.2); */
   color: white;
   font-size: 1.6rem;
+  text-align: right;
 `;
